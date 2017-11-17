9f9c9c96283c2ed8a25b32eeb5679f14ff92d3a2
Ankesh <anki@Ankis-MacBook-Pro.local>
2017-11-17 12:02:29 +0530

    connection.js

--- connection.js
+++ connection.js
@@ -3,7 +3,6 @@ var amqp = require('amqplib/callback_api')
 module.exports = function (cb) {
   amqp.connect('amqp://localhost', function(err, conn, self) {
       if (err) {
-        //throw new Error(err)
         console.log("Error connection"+ JSON.stringify(err));
       }
       cb(conn)
